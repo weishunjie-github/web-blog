706fed5b09170602a2c08e9339699dfa88049cc8
weishunjie-github <157680333@qq.com>
2021-03-16 14:49:45 +0800

    修改地址api

--- src/router/index.js
+++ src/router/index.js
@@ -7,52 +7,52 @@ const routes = [
   {
     path: "/",
     component: () => import('@/components/CommonLayout.vue'),
-    children:[
+    children: [
       {
-        path:'',
-        component:() => import('@/views/Home.vue'),
-        name:'home'
+        path: '',
+        component: () => import('@/views/Home.vue'),
+        name: 'home'
       },
       {
-        path:'/detail/:id',
-        component:() => import('@/views/Detail.vue')
+        path: '/detail/:id',
+        component: () => import('@/views/Detail.vue')
       },
       {
-        path:'/personal',
-        component:() => import('@/views/Personal.vue'),
-        meta:{
-          requireAuth:true//true为这个页面需要登录权限
+        path: '/personal',
+        component: () => import('@/views/Personal.vue'),
+        meta: {
+          requireAuth: true//true为这个页面需要登录权限
         }
       },
       {
-        path:'/article',
-        component:() => import('@/views/Article.vue'),
-        meta:{
-          requireAuth:true//true为这个页面需要登录权限
+        path: '/article',
+        component: () => import('@/views/Article.vue'),
+        meta: {
+          requireAuth: true//true为这个页面需要登录权限
         },
-        name:'article'
+        name: 'article'
       },
       {
-        path:'/article/edit',
-        name:'editArticle',
-        component:() => import('@/views/ArticleEdit.vue'),
-        meta:{
-          requireAuth:true//true为这个页面需要登录权限
+        path: '/article/edit',
+        name: 'editArticle',
+        component: () => import('@/views/ArticleEdit.vue'),
+        meta: {
+          requireAuth: true//true为这个页面需要登录权限
         }
       },
       {
-        path:'/article/edit/:id',
-        name:'updateArticle',
-        component:() => import('@/views/ArticleEdit.vue'),
-        meta:{
-          requireAuth:true//true为这个页面需要登录权限
+        path: '/article/edit/:id',
+        name: 'updateArticle',
+        component: () => import('@/views/ArticleEdit.vue'),
+        meta: {
+          requireAuth: true//true为这个页面需要登录权限
         }
       }
     ]
   },
   {
-    path:'/login',
-    component:() => import('@/views/Login.vue')
+    path: '/login',
+    component: () => import('@/views/Login.vue')
   }
 ];
 
--- src/setAxios.js
+++ src/setAxios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import store from './store/index'
-axios.defaults.baseURL = 'http://127.0.0.1:3000/';
-
+// axios.defaults.baseURL = 'http://127.0.0.1:3000/';
+axios.defaults.baseURL = 'http://192.168.148.129:3000/';
 export default function setAxios() {
     //请求拦截
     axios.interceptors.request.use(
